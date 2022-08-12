a12bdb3d1bb34ee68bedfbabe45b0a70ac84be8d
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-08-12 16:21:03 -0700

    Log Out and Sign Up

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+# Now can decorate any user centric functions with @login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Now can apply LoginRequiredMixin to any CBV
 
 # Add the following import
 
 # Define the home view
+def landing(request):
+  return render(request, 'landing.html')
+
+@login_required
 def home(request):
   return render(request, 'home.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
