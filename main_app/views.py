be9abf5cea223ef9475ef16fc5d7cb071ee461c3
jesusi3 <js508102@gmail.com>
2022-08-14 12:26:39 -0500

    working API

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 # Now can decorate any user centric functions with @login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+import requests
 # Now can apply LoginRequiredMixin to any CBV
 
 # Add the following import
@@ -11,6 +12,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Define the home view
 def landing(request):
   return render(request, 'landing.html')
+
+def activity(request):
+  activity = requests.get('http://www.boredapi.com/api/activity/').json()['activity']
+  return render(request, 'activity.html', {'activity': activity})
 
 @login_required
 def home(request):
@@ -27,7 +32,7 @@ def signup(request):
       user = form.save()
       # This is how we log a user in via code
       login(request, user)
-      return redirect('index')
+      return redirect('home')
     else:
       error_message = 'Invalid sign up - try again'
   # A bad POST or a GET request, so render signup.html with an empty form
