23ab681ce0ee9feaa91407cd15ae7d2ad2154498
jess <jessicamucklow@gmail.com>
2022-08-15 15:26:05 -0700

    save activities in progress

--- main_app/models.py
+++ main_app/models.py
@@ -10,6 +10,7 @@ class Date(models.Model):
     company = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
         return f'{self.title} ({self.id})'
     def get_absolute_url(self):
