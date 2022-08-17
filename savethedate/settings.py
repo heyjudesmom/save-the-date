ca235c548903585b2aa58c04c4cc1ab929f53a1b
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-08-17 13:08:39 -0700

    Config deployment

--- savethedate/settings.py
+++ savethedate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
 import environ
 
 environ.Env()
@@ -131,3 +132,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/dates/'
 LOGOUT_REDIRECT_URL = '/'
 
+django_on_heroku.settings(locals())
